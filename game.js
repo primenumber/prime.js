19dc349b8d304839f543d1cee8d44dd9df9b4a61
prime <prime@kmc.gr.jp>
2018-04-04 02:43:59 +0900

    Implement launching cat hand

--- game.js
+++ game.js
@@ -1,27 +1,59 @@
+// settings
+backgroundColor = 0xFFFFFF;
+
 // load
-loadImages(["cat.png"]);
+loadImages([
+  "cat.png",
+  "cat_hand.png"
+]);
 
 // init
 let cat;
 let hello;
+let hands = [];
 function init() {
   cat = ImageObject("cat.png");
+  cat.anchor.set(0.5, 0.5);
+  cat.x = 400;
+  cat.y = 400;
   addObject(cat);
   hello = TextObject("Hello, World!", {fill: 'white'});
   addObject(hello);
 }
 
-const velocity = 3;
+const cat_v = 3;
 
 // main loop
+let cooltime = 0;
 function mainloop(delta) {
   if (isKeyDown("ArrowRight")) {
-    cat.x += velocity;
+    cat.x += cat_v;
   } if (isKeyDown("ArrowLeft")) {
-    cat.x -= velocity;
+    cat.x -= cat_v;
   } if (isKeyDown("ArrowUp")) {
-    cat.y -= velocity;
+    cat.y -= cat_v;
   } if (isKeyDown("ArrowDown")) {
-    cat.y += velocity;
+    cat.y += cat_v;
   }
+  if (isKeyDown(" ") && cooltime <= 0) {
+    vx = [-6, 0, 6];
+    vy = [-8, -10, -8];
+    for (i = 0; i < 3; ++i) {
+      let new_hand = ImageObject("cat_hand.png");
+      new_hand.anchor.set(0.5, 0.5);
+      new_hand.x = cat.x;
+      new_hand.y = cat.y;
+      new_hand.vx = vx[i];
+      new_hand.vy = vy[i];
+      addObject(new_hand);
+      hands.push(new_hand);
+    }
+    cooltime = 10;
+  } else {
+    cooltime--;
+  }
+  hands.forEach((hand) => {
+    hand.x += hand.vx;
+    hand.y += hand.vy;
+  });
 }
