a2bcccf73d05771a5ea95c2d694a952349f6edc0
prime <prime@kmc.gr.jp>
2018-04-05 01:16:38 +0900

    Implement mouse

--- game.js
+++ game.js
@@ -4,13 +4,15 @@ backgroundColor = 0xFFFFFF;
 // load
 loadImages([
   "cat.png",
-  "cat_hand.png"
+  "cat_hand.png",
+  "mouse.png"
 ]);
 
 // init
 let cat;
 let hello;
 let hands = [];
+let mouses = [];
 function init() {
   cat = ImageObject("cat.png");
   cat.anchor.set(0.5, 0.5);
@@ -25,6 +27,7 @@ const cat_v = 3;
 
 // main loop
 let cooltime = 0;
+let cooltime_mouse = 0;
 function mainloop(delta) {
   if (isKeyDown("ArrowRight")) {
     cat.x += cat_v;
@@ -45,6 +48,7 @@ function mainloop(delta) {
       new_hand.y = cat.y;
       new_hand.vx = vx[i];
       new_hand.vy = vy[i];
+      new_hand.hp = 1;
       addObject(new_hand);
       hands.push(new_hand);
     }
@@ -55,5 +59,38 @@ function mainloop(delta) {
   hands.forEach((hand) => {
     hand.x += hand.vx;
     hand.y += hand.vy;
+  });
+  if (cooltime_mouse <= 0) {
+    let new_mouse = ImageObject("mouse.png");
+    new_mouse.x = Math.random() * (800 - new_mouse.width);
+    new_mouse.y = -100;
+    new_mouse.vx = 0;
+    new_mouse.vy = 5;
+    new_mouse.hp = 3;
+    addObject(new_mouse);
+    mouses.push(new_mouse);
+    cooltime_mouse = 60;
+  } else {
+    cooltime_mouse--;
+  }
+  mouses.forEach((mouse) => {
+    mouse.x += mouse.vx;
+    mouse.y += mouse.vy;
+    hands.forEach((hand) => {
+      if (hitTestRectangle(mouse, hand)) {
+        mouse.hp--;
+        hand.hp--;
+      }
+    });
+  });
+  hands = hands.filter((hand) => {
+    const result = hand.y > -100 && hand.hp > 0;
+    if (!result) removeObject(hand);
+    return result;
+  });
+  mouses = mouses.filter((mouse) => {
+    const result = mouse.y < 700 && mouse.hp > 0;
+    if (!result) removeObject(mouse);
+    return result;
   });
 }
