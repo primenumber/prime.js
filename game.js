6cf4a055a3164334ea42230562b5a13c7ae954be
primenumber <prime@kmc.gr.jp>
2018-04-05 22:23:16 +0900

    Add 'fps'

--- game.js
+++ game.js
@@ -35,7 +35,7 @@ const cat_v = 3;
 let cooltime = 0;
 let cooltime_mouse = 0;
 function mainloop(delta) {
-  frame_rate_txt.text = "Frame rate: " + Math.round(60/delta).toString();
+  frame_rate_txt.text = "Frame rate: " + Math.round(60/delta).toString() + "fps";
   if (isKeyDown("ArrowRight")) {
     cat.x += cat_v;
   } if (isKeyDown("ArrowLeft")) {
